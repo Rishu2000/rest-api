cdfa7e659cf9df839f8a615bc3b95a181ef2da7e
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-28 20:57:46 +0530

    implemented morgan

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const root = require('./routers/root');
 const port = 3001;
 
+app.use(morgan("dev"));
 app.use('/',root);
 
 app.listen(port, () => { 
