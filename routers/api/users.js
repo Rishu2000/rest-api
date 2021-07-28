cf0ba0ca283d0c5e13fcfb14487245ccf82d227d
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-28 21:17:15 +0530

    Made password secure.

--- routers/api/users.js
+++ routers/api/users.js
@@ -3,12 +3,18 @@ const app = express();
 const users = require('../../constants/users');
 
 app.get('/users', (req, res) =>{
-    res.json(users)
+    res.json(users.map((u,UserId) => {
+        const a = {UserId, ...u};
+        delete a.Password;
+        return a;
+    }))
 })
 app.get('/users/:id', (req, res) =>{
     const index = +req.params.id;
     if(users[index]){
-        res.json(users[index]);
+        const a = {"UserId": index, ...users[index]}
+        delete a.Password;
+        res.json(a);
     }else{
         res.status(404).json("Not Found");
     }
