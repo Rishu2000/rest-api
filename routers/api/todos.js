31f4b48dbbf39b198f71ac0dfecfe51590961ed4
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-28 20:54:42 +0530

    get the data by id.

--- routers/api/todos.js
+++ routers/api/todos.js
@@ -5,5 +5,13 @@ const todos = require('../../constants/todos');
 app.get('/todos', (req, res) =>{
     res.json(todos)
 })
+app.get('/todos/:id', (req, res) =>{
+    const index = +req.params.id;
+    if(todos[index]){
+        res.json(todos[index]);
+    }else{
+        res.status(404).json("Not Found");
+    }
+})
 
 module.exports = app;
--- routers/api/users.js
+++ routers/api/users.js
@@ -5,5 +5,13 @@ const users = require('../../constants/users');
 app.get('/users', (req, res) =>{
     res.json(users)
 })
+app.get('/users/:id', (req, res) =>{
+    const index = +req.params.id;
+    if(users[index]){
+        res.json(users[index]);
+    }else{
+        res.status(404).json("Not Found");
+    }
+})
 
 module.exports = app;
