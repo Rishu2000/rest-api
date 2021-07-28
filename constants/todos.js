726550e03e7d3b7d97d6b717339c852135fe1957
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-28 19:59:26 +0530

    Created some constants.

---
+++ constants/todos.js
@@ -0,0 +1,6 @@
+module.exports = [
+    "Milk",
+    "Curd",
+    "Suger",
+    "Banana",
+]
---
+++ constants/users.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "Name":"Rishav Kumar Shah",
+        "Password":"hello123",
+        "LinkedIn":"https://www.linkedin.com/feed/"
+    },{
+        "Name":"Ankit Kumar",
+        "Password":"ank123",
+        "LinkedIn":"https://www.linkedin.com/feed/ank"
+    },{
+        "Name":"Sanu Kumar",
+        "Password":"san123",
+        "LinkedIn":"https://www.linkedin.com/feed/san"
+    },{
+        "Name":"Aryan Kumar",
+        "Password":"ary123",
+        "LinkedIn":"https://www.linkedin.com/feed/ary"
+    },{
+        "Name":"Praduman Kumar",
+        "Password":"prad123",
+        "LinkedIn":"https://www.linkedin.com/feed/prad"
+    }
+]
