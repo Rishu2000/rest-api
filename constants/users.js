e1bb72431fd920e26ce74b1ad641bd8d95b71042
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-29 16:20:17 +0530

    Added another field in the constants as Username.

--- constants/users.js
+++ constants/users.js
@@ -1,21 +1,26 @@
 module.exports = [
     {
+        "Username": "Rishav",
         "Name":"Rishav Kumar Shah",
         "Password":"hello123",
         "LinkedIn":"https://www.linkedin.com/feed/"
     },{
+        "Username": "Ankit",
         "Name":"Ankit Kumar",
         "Password":"ank123",
         "LinkedIn":"https://www.linkedin.com/feed/ank"
     },{
+        "Username": "Sanu",
         "Name":"Sanu Kumar",
         "Password":"san123",
         "LinkedIn":"https://www.linkedin.com/feed/san"
     },{
+        "Username": "Aryan",
         "Name":"Aryan Kumar",
         "Password":"ary123",
         "LinkedIn":"https://www.linkedin.com/feed/ary"
     },{
+        "Username": "Praduman",
         "Name":"Praduman Kumar",
         "Password":"prad123",
         "LinkedIn":"https://www.linkedin.com/feed/prad"
