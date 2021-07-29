23de919fc7a3fb815502aeb54ae4a1b7a2f5b4bd
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-29 06:24:24 +0530

    Implemented cors.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 const root = require('./routers/root');
 const port = 3001;
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use('/',root);
 
