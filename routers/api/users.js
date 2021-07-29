de9645877c49578fa409b2d0de6aff8f8c551dcb
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-29 16:22:29 +0530

    Added login and little Authentication functionality.

--- routers/api/users.js
+++ routers/api/users.js
@@ -2,12 +2,37 @@ const express = require('express');
 const app = express();
 const users = require('../../constants/users');
 
+let Authentication = false;
+
+app.use(express.json());
 app.get('/users', (req, res) =>{
-    res.json(users.map((u,UserId) => {
-        const a = {UserId, ...u};
-        delete a.Password;
-        return a;
-    }))
+    if(Authentication){
+        res.json(users.map((u,UserId) => {
+            const a = {UserId, ...u};
+            delete a.Password;
+            return a;
+        }))
+    }else{
+        res.status(401).json("Please login to see.")
+    }
+})
+app.get('/login', (req, res) => {
+    res.json({Authentication});
+})
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+    if(!username || !password){
+        res.status(403).json("Please enter a username and password both.");
+    }else{
+        const match = users.filter((u) => u.Username.toLowerCase() === username.toLowerCase() && u.Password === password);
+        if(match.length == 1){
+            Authentication = true;
+            res.json("You are in.");
+        }else{
+            Authentication = false;
+            res.status(404).json("Wrong");
+        }
+    }
 })
 app.get('/users/:id', (req, res) =>{
     const index = +req.params.id;
